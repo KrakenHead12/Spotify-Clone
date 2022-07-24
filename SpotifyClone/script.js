a95bdf688ec8848d4c27d583f46c57c223dd1380
KrakenHead12 <85258581+KrakenHead12@users.noreply.github.com>
2022-07-25 02:36:06 +0530

    Add files via upload

---
+++ SpotifyClone/script.js
@@ -0,0 +1,12 @@
+console.log("WELCOME TO SPOTIFY");
+let audioElement = new Audio('CastleOfGlass.mp3');
+let songs = [
+	{songName: "Castle Of Glass", filePath:"song/CastleOfGlass.mp3", coverPath:"covers/1.jpg" },
+	{songName: "Castle Of Glass", filePath:"song/CastleOfGlass.mp3", coverPath:"covers/1.jpg" },
+	{songName: "Castle Of Glass", filePath:"song/CastleOfGlass.mp3", coverPath:"covers/1.jpg" },
+	{songName: "Castle Of Glass", filePath:"song/CastleOfGlass.mp3", coverPath:"covers/1.jpg" },
+	{songName: "Castle Of Glass", filePath:"song/CastleOfGlass.mp3", coverPath:"covers/1.jpg" },
+	{songName: "Castle Of Glass", filePath:"song/CastleOfGlass.mp3", coverPath:"covers/1.jpg" }
+]
+
+//audioElement.play(); r
